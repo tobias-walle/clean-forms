9192a0b5fedb3606e0828c650ce52cb429216bc6
Tobias Walle <tobias.walle@maibornwolff.de>
2018-01-23 08:54:20 +0100

    Create seperate tsconfigs for each env

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-  require('../dist/stories');
+  require('../out/storybook/stories');
 }
 
 configure(loadStories, module);
