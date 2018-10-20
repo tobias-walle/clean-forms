d0610935d5298815a9333e1327089e3122999790
Tobias Walle <tobias.walle@maibornwolff.de>
2018-10-20 14:58:23 +0200

    fix: invalid typings

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,1 @@
-export type Diff<T extends keyof any, U extends keyof any> = (
-  { [P in T]: P } & {[P in U]: never } & { [x: string]: never }
-)[T];
-
-export type Omit<T, K extends string> = Pick<T, Diff<keyof T, K>>;
+export type Omit<T, K extends string> = Pick<T, Exclude<keyof T, K>>;
