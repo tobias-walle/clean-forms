1fc0edfabc3414dd11bced10327fd96bddad2a17
Tobias Walle <tobias.walle@maibornwolff.de>
2019-04-06 20:23:51 +0200

    remove storybook

--- .storybook/addons.js
+++
@@ -1,2 +0,0 @@
-import '@storybook/addon-actions/register';
-import '../out/storybook/stories/StateWrapper/register';
--- .storybook/config.js
+++
@@ -1,7 +0,0 @@
-import { configure } from '@storybook/react';
-
-function loadStories() {
-  require('../out/storybook/stories/index');
-}
-
-configure(loadStories, module);
