bd3d3b22152b984c13e678328a995c24d590ae71
Tobias Walle <tobias.walle@maibornwolff.de>
2019-10-30 14:02:14 +0100

    fix: set default value in getInputProps

--- src/lib/utils/getInputProps.ts
+++ src/lib/utils/getInputProps.ts
@@ -2,13 +2,13 @@ import React from 'react';
 import { InputProps } from '../hocs';
 import { combineProps } from './combineProps';
 
-export function getInputProps<Value extends number | string | string[]>(
-  fieldInputProps: InputProps<Value>,
+export function getInputProps(
+  fieldInputProps: InputProps<string>,
   customProps: React.PropsWithoutRef<JSX.IntrinsicElements['input']> = {}
 ): React.PropsWithoutRef<JSX.IntrinsicElements['input']> {
   const inputProps: JSX.IntrinsicElements['input'] = {
     name: fieldInputProps.name,
-    value: fieldInputProps.value,
+    value: fieldInputProps.value || '',
     onBlur: fieldInputProps.onBlur,
     onChange: event => fieldInputProps.onChange(event.target.value as any),
   };
